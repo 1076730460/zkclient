5b1f67fb019599d294a640476e0d1c43b03b9a15
1076730460 <1076730460@qq.com>
2018-08-04 12:53:00 +0800

    初始化导入

---
+++ src/main/java/com/util/Utils.java
@@ -0,0 +1,13 @@
+package com.util;
+
+public class Utils {
+ public static String mvTest(String arg) {
+	 return arg;
+ }
+ public static void main(String[] args) {
+	System.out.println(args);
+}
+ public void test1() {
+	 System.out.println("hi");
+ }
+}
